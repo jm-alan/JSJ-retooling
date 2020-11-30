5bd98b3ead58964056597a7d9e73db78b6afd132
Joshua Alan <j.maxwell.alan@gmail.com>
2020-11-30 04:58:31 -0500

    Configure as node-env controlled server

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const routes = require('./routes.js');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+app.use((req, res, next) => {
+  const error = new Error('The requested page could not be found.');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    // TODO Log the error to the database.
+  } else {
+    console.error(err);
+  }
+  next(err);
+});
+
+// Error handler for 404 errors.
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404);
+    res.render('page-not-found', {
+      title: 'Page Not Found'
+    });
+  } else {
+    next(err);
+  }
+});
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  const isProduction = process.env.NODE_ENV === 'production';
+  res.render('error', {
+    title: 'Server Error',
+    message: isProduction ? null : err.message,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+module.exports = app;
---
+++ routes.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
+
+const db = require('./db/models');
+
+const router = express.Router();
+
+const csrfProtection = csrf({ cookie: true });
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+  res.render('book-list', { title: 'Books', books });
+}));
+
+router.get('/book/add', csrfProtection, (req, res) => {
+  res.render('book-add', {
+    title: 'Add Book',
+    csrfToken: req.csrfToken()
+  });
+});
+
+const questionValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Title')
+    .isLength({ max: 255 })
+    .withMessage('Title must not be more than 255 characters long'),
+  check('body')
+    .exists({ checkFalsy: true })
+    .withMessage('Body cannot be empty')
+];
+
+router.post('/book/add', csrfProtection, bookValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher
+    } = req.body;
+
+    const book = db.Book.build({
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await book.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('book-add', {
+        title: 'Add Book',
+        book,
+        errors,
+        csrfToken: req.csrfToken()
+      });
+    }
+  }));
+
+router.get('/book/edit/:id(\\d+)', csrfProtection,
+  asyncHandler(async (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    const book = await db.Book.findByPk(bookId);
+    res.render('book-edit', {
+      title: 'Edit Book',
+      book,
+      csrfToken: req.csrfToken()
+    });
+  }));
+
+router.post('/book/edit/:id(\\d+)', csrfProtection, bookValidators,
+  asyncHandler(async (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    const bookToUpdate = await db.Book.findByPk(bookId);
+
+    const {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher
+    } = req.body;
+
+    const book = {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher
+    };
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await bookToUpdate.update(book);
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('book-edit', {
+        title: 'Edit Book',
+        book: {
+          ...book,
+          id: bookId
+        },
+        errors,
+        csrfToken: req.csrfToken()
+      });
+    }
+  })
+);
+
+router.get('/book/delete/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+  const book = await db.Book.findByPk(bookId);
+  res.render('book-delete', {
+    title: 'Delete Book',
+    book,
+    csrfToken: req.csrfToken()
+  });
+}));
+
+router.post('/book/delete/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+  const book = await db.Book.findByPk(bookId);
+  await book.destroy();
+  res.redirect('/');
+}));
+
+module.exports = router;
