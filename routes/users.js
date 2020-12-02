ec4f8a570b76232d1cc0a45e0b57136d0a71eae0
Andrew Dotterer <adotterer@gmail.com>
2020-12-02 09:58:23 -0800

    fix login / signup

--- routes/users.js
+++ routes/users.js
@@ -1,109 +1,127 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const {
   asyncHandler,
   handleValidationErrors,
   getUserToken,
   requireAuth,
   loginUser,
-  logoutUser
-} = require('../utils/server-utils.js');
-const { check, validationResult } = require('express-validator');
-const { User } = require('../db/models');
-const crsf = require('csurf');
+  logoutUser,
+} = require("../utils/server-utils.js");
+const { check, validationResult } = require("express-validator");
+const { User } = require("../db/models");
+const crsf = require("csurf");
 const crsfProtection = crsf({ cookie: true });
 
 const userValidator = [
-  check('userName')
+  check("userName")
     .exists({ checkFalsy: true })
     .custom(async (value) => {
-      if (await User.findOne({ where: { userName: value } })) { throw new Error('The provided user name is already in use.'); } else {
-        const invalidCharacters = '!?~`@#$%^&*(){}\\/<>,[]|';
+      if (await User.findOne({ where: { userName: value } })) {
+        throw new Error("The provided user name is already in use.");
+      } else {
+        const invalidCharacters = "!?~`@#$%^&*(){}\\/<>,[]|";
         for (const letter of value) {
-          if (invalidCharacters.includes(letter)) { throw new Error('User name contains invalid character.'); }
+          if (invalidCharacters.includes(letter)) {
+            throw new Error("User name contains invalid character.");
+          }
         }
         return true;
       }
     }),
-  check('email')
+  check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.')
+    .withMessage("Please provide a valid email.")
     .custom(async (value) => {
-      if (await User.findOne({ where: { email: value } })) { throw new Error('The provided email is already in use.'); } else return true;
+      if (await User.findOne({ where: { email: value } })) {
+        throw new Error("The provided email is already in use.");
+      } else return true;
     }),
-  check('password')
+  check("password")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
-  check('confirmPassword')
+    .withMessage("Please provide a password."),
+  check("confirmPassword")
     .exists({ checkFalsy: true })
-    .withMessage('Please confirm password.')
+    .withMessage("Please confirm password.")
     .custom((value, { req }) => {
-      if (value !== req.body.password) { throw new Error('Confirm Password does not match Password.'); } else return true;
+      if (value !== req.body.password) {
+        throw new Error("Confirm Password does not match Password.");
+      } else return true;
     }),
-  check('firstName')
+  check("firstName")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a first name.'),
-  check('lastName')
+    .withMessage("Please provide a first name."),
+  check("lastName")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a last name.')
+    .withMessage("Please provide a last name."),
 ];
 
 const loginValidator = [
-  check('identification')
+  check("identification")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a username or email.')
+    .withMessage("Please provide a username or email.")
     .custom(async (value) => {
       if (value) {
         if (String(value).match(/@/g)) {
           if (!(await User.findOne({ where: { email: value } }))) {
-            throw new Error('Invalid login.');
+            throw new Error("Invalid login.");
           } else return true;
         } else {
           if (!(await User.findOne({ where: { userName: value } }))) {
-            throw new Error('Invalid login.');
+            throw new Error("Invalid login.");
           } else return true;
         }
       }
     }),
-  check('password')
+  check("password")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.')
+    .withMessage("Please provide a password.")
     .custom(async (value, { req }) => {
       if (value) {
         if (String(value).match(/@/g)) {
           const user = await User.findOne({
-            where: { email: req.body.identification }
+            where: { email: req.body.identification },
           });
-          if (!user) throw new Error('Invalid login.');
+          if (!user) throw new Error("Invalid login.");
           else if (
-            !bcrypt.compareSync(req.body.password, user.hashedPassword.toString())
-          ) { throw new Error('Invalid login.'); }
+            !bcrypt.compareSync(
+              req.body.password,
+              user.hashedPassword.toString()
+            )
+          ) {
+            throw new Error("Invalid login.");
+          }
         } else {
           const user = await User.findOne({
-            where: { userName: req.body.identification }
+            where: { userName: req.body.identification },
           });
-          if (!user) throw new Error('Invalid login.');
+          if (!user) throw new Error("Invalid login.");
           else if (
-            !bcrypt.compareSync(req.body.password, user.hashedPassword.toString())
-          ) { throw new Error('Invalid login.'); }
+            !bcrypt.compareSync(
+              req.body.password,
+              user.hashedPassword.toString()
+            )
+          ) {
+            throw new Error("Invalid login.");
+          }
         }
       }
-    })
+    }),
 ];
 
-router.get('/', crsfProtection, (req, res) => {
+router.get("/", crsfProtection, (req, res) => {
   // login pug file
-  res.render('signup', { title: 'Login', errors: ['this is messed up', 'test error'], csrfToken: req.csrfToken() });
+  res.render("signup", { title: "Sign Up", csrfToken: req.csrfToken() });
 });
 
-router.get('/login', crsfProtection, (req, res) => {
-  res.render('login', { csrfToken: req.csrfToken() });
+router.get("/login", crsfProtection, (req, res) => {
+  res.render("login", { title: "Log In", csrfToken: req.csrfToken() });
 });
 
 router.post(
-  '/login',
+  "/login",
   crsfProtection,
   loginValidator,
 
@@ -111,16 +129,20 @@ router.post(
     const validatorErrors = validationResult(req);
     if (validatorErrors.isEmpty()) {
       loginUser(req, res, user);
-      res.redirect('/');
+      res.redirect("/");
     } else {
-      const errors = validatorErrors.array().map(err => err.msg);
-      res.render('login', { errors, csrfToken: req.csrfToken() });
+      const errors = validatorErrors.array().map((err) => err.msg);
+      res.render("login", {
+        title: "Log In",
+        errors,
+        csrfToken: req.csrfToken(),
+      });
     }
   }
 );
 
 router.post(
-  '/',
+  "/",
   crsfProtection,
   userValidator,
   asyncHandler(async (req, res, next) => {
@@ -134,20 +156,24 @@ router.post(
         email,
         hashedPassword,
         firstName,
-        lastName
+        lastName,
       });
       loginUser(req, res, user);
-      res.redirect('/');
+      res.redirect("/");
     } else {
-      const errors = validatorErrors.array().map(err => err.msg);
-      res.render('signup', { title: 'Login', errors, csrfToken: req.csrfToken() });
+      const errors = validatorErrors.array().map((err) => err.msg);
+      res.render("signup", {
+        title: "Sign Up",
+        errors,
+        csrfToken: req.csrfToken(),
+      });
     }
   })
 );
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   logoutUser(req, res);
-  res.redirect('/login');
+  res.redirect("/login");
 });
 
 module.exports = router;
