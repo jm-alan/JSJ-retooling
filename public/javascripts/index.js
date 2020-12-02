6e10c1584b65c2f60e23444c6b4fde6684ba565d
Andrew Dotterer <adotterer@gmail.com>
2020-12-02 15:05:23 -0800

    start page buttons

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,35 @@
-window.addEventListener('load', (event) => {
-  console.log('hello from javascript!');
+const array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+
+let currentPage = 1;
+
+const navButton = (text, container) => {
+  const btn = document.createElement("button");
+  btn.classList.add("numberedButton");
+  btn.innerText = `${text}`;
+  container.appendChild(btn);
+};
+
+window.addEventListener("load", (event) => {
+  let container = document.getElementById("pageSelection");
+  const totalPages = Math.ceil(array.length / 10);
+  if (totalPages > 1) {
+    navButton("Prev", container);
+    for (let i = 1; i <= totalPages; i++) {
+      navButton(i, container);
+    }
+    navButton("Next", container);
+  }
+
+  container.addEventListener("click", (event) => {
+    const target = event.target.innerText;
+    if (target === "Prev" && currentPage !== 1) {
+      // fetch new info at current page - 1
+    } else if (target === "Next" && totalPages !== currentPage) {
+      // fetch new info at current page + 1
+    } else {
+      if (currentPage !== target) {
+        // fetch info page
+      }
+    }
+  });
 });
