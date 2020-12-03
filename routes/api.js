164bf2c4d55b77a82fcc33f86e8b82a3a98f5c26
Joshua <j.maxwell.alan@gmail.com>
2020-12-03 09:44:32 -0500

    api routes configured

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { mostPopular, mostRecent, getThreadById } = require('../utils/queryFunctions');
+const { asyncHandler } = require('../utils/server-utils');
 
 router.get('/threads', (req, res) => {
-  console.log(req.query.banana.split(','))
+  const threads = [];
+  req.query.list.split(',')
+    .forEach(async id => {
+      threads.push(await getThreadById(id));
+    });
+  res.json({ threadObjects });
 });
+
+router.get('/recent', asyncHandler(async (req, res) => {
+  const threads = await mostRecent();
+  res.json({ threads });
+}));
+
+router.get('/popular', asyncHandler(async (req, res) => {
+  const threads = await mostPopular();
+  res.json({ threads });
+}));
 
 module.exports = router;
