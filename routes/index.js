b8e0b53db9c616dc6b22ff826a339f47ca9a5427
Joshua <j.maxwell.alan@gmail.com>
2020-12-03 21:55:21 -0500

    Correct unnecessary 'threadId' reference in threadPage.pug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get('/', function (req, res, next) {
   // const obj = {
   //   title: 'This is a JS post!',
   //   threadId: 14,
@@ -21,18 +21,18 @@ router.get("/", function (req, res, next) {
   //   score: 1,
   //   timeStamp: 'created 5 mins ago by '
   // };
-  res.render("index", {
-    title: "Javascript Jungle",
+  res.render('index', {
+    title: 'Javascript Jungle'
     // questions: [obj2, obj, obj, obj, obj, obj, obj, obj, obj, obj]
   });
 });
 
-router.get("/login", function (req, res, next) {
-  res.redirect("/users/login");
+router.get('/login', function (req, res, next) {
+  res.redirect('/users/login');
 });
 
-router.get("/signup", function (req, res, next) {
-  res.redirect("/users");
+router.get('/signup', function (req, res, next) {
+  res.redirect('/users');
 });
 
 module.exports = router;
