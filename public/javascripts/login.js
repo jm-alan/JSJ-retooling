942588849d3df23f9695ac984c8257897708edec
Joshua <j.maxwell.alan@gmail.com>
2020-12-04 02:19:41 -0500

    formatting

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", (event) => {
-  const loginButton = document.getElementById("demoUser");
-  loginButton.addEventListener("click", async (event) => {
+window.addEventListener('load', (event) => {
+  const loginButton = document.getElementById('demoUser');
+  loginButton.addEventListener('click', async (event) => {
     event.preventDefault();
-    const usernameInput = document.getElementById("username");
-    const passwordInput = document.getElementById("password");
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
 
-    usernameInput.value = "demo_user";
-    passwordInput.value = "password";
+    usernameInput.value = 'demo_user';
+    passwordInput.value = 'password';
   });
 });
