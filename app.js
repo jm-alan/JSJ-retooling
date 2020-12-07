d4719723d47c9ae142f43f446739c9f117ca7a9d
JM Alan <j.maxwell.alan@gmail.com>
2020-12-07 11:03:00 -0500

    minor fixing

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
