85d78155308be82385dba178eca7837002dcf1c9
JM <j.maxwell.alan@gmail.com>
2021-03-09 21:03:05 -0500

    path.resolve > string concat __dirname

--- db/seeders/default-posts.js
+++ db/seeders/default-posts.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const postObjs = [];
 
 for (let i = 1; i <= 100; i++) {
-  const { body, score } = JSON.parse(fs.readFileSync(`${__dirname}/fetch/bulkData/questions-and-threads/question${i}.txt`, 'utf-8'));
+  const { body, score } = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fetch/bulkData/questions-and-threads/question${i}.txt`), 'utf-8'));
   postObjs.push({ body, userId: Math.ceil(Math.random() * 3), threadId: i, isQuestion: true, score });
 }
 
 for (let i = 1; i <= 100; i++) {
-  const { answers } = JSON.parse(fs.readFileSync(`${__dirname}/fetch/bulkData/questions-and-threads/question${i}.txt`, 'utf-8'));
+  const { answers } = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fetch/bulkData/questions-and-threads/question${i}.txt`), 'utf-8'));
   answers.forEach(({ body, score }) => {
     postObjs.push({ body, userId: Math.ceil(Math.random() * 3), threadId: i, isQuestion: false, score });
   });
