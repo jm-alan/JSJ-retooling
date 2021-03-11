0771c98766a99bc7a9168339d001716eaf11a526
JM <j.maxwell.alan@gmail.com>
2021-03-11 02:34:41 -0500

    change search address to query directly on home, eliminating the need for a dedicated search page

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,18 +1,18 @@
-window.addEventListener('load', async (event) => {
+window.addEventListener('load', () => {
   const bar = document.getElementById('searchBar');
-  bar.addEventListener('keypress', async (event) => {
+  bar.addEventListener('keypress', (event) => {
     if (event.key === 'Enter' && bar.value !== '' && bar.value !== ' ') {
       const searchData = bar.value;
-      const url = `/search?entry=${searchData}`;
+      const url = `/?entry=${searchData}`;
       window.location.href = url;
     }
   });
   const icon = document.getElementById('searchIcon');
 
-  icon.addEventListener('click', async (event) => {
+  icon.addEventListener('click', () => {
     if (bar.value !== '' && bar.value !== ' ') {
       const searchData = bar.value;
-      const url = `/search?entry=${searchData}`;
+      const url = `/?entry=${searchData}`;
       window.location.href = url;
     }
   });
