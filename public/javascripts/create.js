a91c47c9931557586f8feb33c51365eb47909877
Joshua <j.maxwell.alan@gmail.com>
2021-04-16 13:39:04 -0400

    unnecessary hard string coersion for internal-only function

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,6 +1,6 @@
 export default function create (type, id = null, ...classes) {
-  const el = document.createElement(String(type));
-  if (id) el.setAttribute('id', String(id));
+  const el = document.createElement(type);
+  if (id) el.setAttribute('id', id);
   if (classes.length) classes.forEach(hClass => el.classList.add(hClass));
   return el;
 }
