596edea42a36204dc28a8467b30e751613db48c4
Joshua <j.maxwell.alan@gmail.com>
2021-08-20 22:11:40 -0400

    add html and css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    publicPath: './public',
+    publicPath: '/',
     filename: '[name].js'
   },
   target: 'node',
@@ -28,6 +28,14 @@ module.exports = {
             transforms: ['imports']
           }
         }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
       }
     ]
   }
