8ebfb1c2a06b89853c5009c97d2d80e79c0d4ea2
Joshua <j.maxwell.alan@gmail.com>
2021-08-20 23:59:14 -0400

    only next if file not found

--- app.js
+++ app.js
@@ -27,9 +27,10 @@ const resolveStaticJS = (req, res, next) => {
   const urlMatch = req.url.match(/^\/(index|home|login|interactivity)\.js$/);
   const filename = urlMatch && urlMatch[0].slice(1);
   if (filename) {
-    res.sendFile(resolve(__dirname, filename));
-  }
-  next();
+    const resolvedPath = resolve(__dirname, filename);
+    console.log('RESOLVED PATH==============', resolvedPath);
+    res.sendFile(resolvedPath);
+  } else next();
 };
 
 const staticPath = environment === 'development'
