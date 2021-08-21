e7d05b7213dddef13d4eaadb69e9a813c2b70022
Joshua <j.maxwell.alan@gmail.com>
2021-08-20 21:36:34 -0400

    translate post model to sequelize 6

--- db/models/post.js
+++ db/models/post.js
@@ -1,15 +1,29 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
+
+module.exports = (sequelize, { DataTypes, Model }) => {
+  class Post extends Model {
+    static associate ({ Thread, Score, User }) {
+      Post.hasMany(Score, { foreignKey: 'postId', onDelete: 'CASCADE' });
+      Post.belongsTo(Thread, { foreignKey: 'threadId' });
+      Post.belongsTo(User, { foreignKey: 'userId' });
+      Post.belongsToMany(User, {
+        through: Score,
+        foreignKey: 'postId',
+        otherKey: 'userId'
+      });
+    }
+  }
+
+  Post.init({
     body: DataTypes.TEXT,
     userId: DataTypes.INTEGER,
     threadId: DataTypes.INTEGER,
     isQuestion: DataTypes.BOOLEAN,
     score: DataTypes.INTEGER
-  }, {});
-  Post.associate = function (models) {
-    Post.belongsTo(models.Thread, { foreignKey: 'threadId' });
-    Post.belongsTo(models.User, { foreignKey: 'userId' });
-  };
+  }, {
+    sequelize,
+    modelName: 'Post'
+  });
+
   return Post;
 };
